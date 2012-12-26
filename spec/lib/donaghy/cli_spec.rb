e00998d418638463ff8c4bcfc7844858e14412ba
Topper Bowers <topper@toppingdesign.com>
2012-12-26 12:25:18 -0500

    run donaghy from the command line

--- spec/lib/donaghy/cli_spec.rb
+++ spec/lib/donaghy/cli_spec.rb
@@ -9,9 +9,30 @@ module Donaghy
 
       it "should load the console" do
         Binding.any_instance.should_receive(:pry).and_return(true)
-        CLI.new(argv)
+        CLI.new(argv).parse
       end
     end
+
+    describe "run" do
+      let(:argv) { %w(run) }
+
+      it "should start the server" do
+        Donaghy.server.should_receive(:start).and_return(true)
+        cli = nil
+        Thread.new do
+          cli = CLI.new(argv)
+          cli.parse
+        end
+        #wait until the other thread is doing stuff
+        until cli
+          puts 'no cli'
+          sleep 0.1
+        end
+        ->() { cli.interrupt }.should raise_error(Interrupt)
+      end
+
+    end
+
     describe "publish" do
       let(:event_path) { "path/to/event" }
       let(:payload) { "some_payload" }
@@ -19,7 +40,7 @@ module Donaghy
 
       it "should publish an event" do
         Donaghy.event_publisher.should_receive(:root_trigger).with(event_path, payload: payload).and_return(true)
-        CLI.new(argv)
+        CLI.new(argv).parse
       end
     end
 
@@ -28,7 +49,7 @@ module Donaghy
 
       it "should QueueFinder.list_all" do
         QueueFinder.should_receive(:all_listeners).and_return "testOutputFromListAll"
-        CLI.new(argv)
+        CLI.new(argv).parse
       end
     end
   end
