777bacf53767798b2dd340ec3815eec50070bf37
Topper Bowers <topper@toppingdesign.com>
2013-01-09 12:36:43 -0500

    event publisher should default to triggering from path_with_root instead of the event_path

--- lib/donaghy/event_publisher.rb
+++ lib/donaghy/event_publisher.rb
@@ -2,6 +2,13 @@ module Donaghy
   class EventPublisher
     include Donaghy::Service
 
+    # overwrite the local trigger here because we never want /root/event_publisher/path
+    # but we do want to default to including the root
+    def trigger(path, opts = {})
+      logger.info "#{self.class.name} is triggering: #{path_with_root(path)} with #{opts.inspect}"
+      global_publish(path_with_root(path), opts)
+    end
+
     #opts takes host and id
     def ping(root, service, reply_to, opts = {})
       host = opts[:host]
