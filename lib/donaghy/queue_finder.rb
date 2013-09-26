39e4e18a6f1a7aa4e52954455e2ba20c2e0cc517
Devon Hinton <dhinton@stanford.edu>
2013-09-26 11:02:31 -0400

    Addded logging to the queuefinder

--- lib/donaghy/queue_finder.rb
+++ lib/donaghy/queue_finder.rb
@@ -23,8 +23,17 @@ module Donaghy
     # we need to optimize this - but there ain't no event paths right now
     def matching_paths
       event_paths = storage.get("donaghy_event_paths")
+      if (path == "approvedReasons" || path == :approvedReasons)
+        logger.warn("approved reasons recieved and event paths are #{event_paths}")
+      end
       if event_paths and event_paths.respond_to?(:select)
+        if (path == "approvedReasons" || path == :approvedReasons)
+          logger.warn("Inside the first if block and about to match")
+        end
         event_paths.select do |registered_path|
+          if (path == "approvedReasons" || path == :approvedReasons)
+            logger.warn("attempting to match with #{registered_path}")
+          end
           File.fnmatch(registered_path, path)
         end
       else
