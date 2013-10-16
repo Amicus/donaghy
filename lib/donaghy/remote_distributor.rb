31877a914b7ccd801d26f23467cfd01f73ae79dd
Topper Bowers <topper@toppingdesign.com>
2013-10-16 10:30:27 -0400

    moar logging

--- lib/donaghy/remote_distributor.rb
+++ lib/donaghy/remote_distributor.rb
@@ -3,13 +3,18 @@ module Donaghy
     include Logging
 
     def handle_distribution(evt)
-      matching_queue_names(evt.path).each do |queue|
-        logger.info("sending #{evt.path} to #{queue}")
+      queue_names = matching_queue_names(evt.path)
+      logger.info("RemoteDistributor: matching queues #{queue_names.inspect}")
+      # if queue_names.empty? put in the "no one is listening" queue
+      queue_names.each do |queue|
+        logger.info("RemoteDistributor: sending #{evt.path} to #{queue}")
         Donaghy.queue_for(queue).publish(evt)
       end
     end
+
     def matching_queue_names(path)
       queue_and_class_name = QueueFinder.new(path, Donaghy.storage).find
+      logger.info("RemoteDistributor: all queue and class names #{queue_and_class_name}")
       names = if !queue_and_class_name.empty?
                 queue_and_class_name.map{|obj| obj[:queue]}.uniq
               else
@@ -17,5 +22,6 @@ module Donaghy
               end
       names
     end
+
   end
 end
