9a1bf32c1ddb3a1000f7e8ab38734339aa56e917
Topper Bowers <topper@toppingdesign.com>
2014-02-01 11:50:49 -0500

    add a low priority method to the event publisher, possible future improvements could include publishing to a queue and then bulk publishing the messages.

--- lib/donaghy/event_publisher.rb
+++ lib/donaghy/event_publisher.rb
@@ -2,5 +2,32 @@ module Donaghy
   class EventPublisher
     include Donaghy::Service
 
+    class LowPriorityPublisher
+      include Donaghy::Service
+      include Celluloid
+    end
+    
+    attr_reader :supervisor
+    def initialize
+      @supervisor = LowPriorityPublisher.supervise
+    end
+
+    def low_priority_trigger(path, opts = {})
+      actor.async.root_trigger(path, opts)
+    end
+
+    def stop
+      supervisor.terminate if supervisor.alive?
+    end
+
+    def alive?
+      supervisor.alive?
+    end
+
+  private
+    def actor
+      supervisor.actors.first
+    end
+
   end
 end
